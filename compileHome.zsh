c64db34af1f47181578332e17be20a3b557ad2e5
Zak Waddle <zakwaddle@gmail.com>
2023-06-24 01:13:33 -0400

    working on .mpy compiled version of home package

---
+++ compileHome.zsh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Source and destination directories
+src_dir="/Users/zakwaddle/MQTT-Log/MQTT-Firmware/Firmware/HomeModule/home"
+dest_dir="/Users/zakwaddle/MQTT-Log/MQTT-Firmware/Firmware/HomeModule/mpy/home"
+
+# Check if destination directory exists, create if not
+if [ ! -d "$dest_dir" ]; then
+  mkdir -p "$dest_dir"
+fi
+
+# Compile all .py files to .mpy and move to destination directory
+find "$src_dir" -name '*.py' | while read -r file
+do
+    dest_file="${dest_dir}${file#$src_dir}"
+    dest_path=$(dirname "$dest_file")
+
+    mkdir -p "$dest_path"
+    mpy-cross "$file" -o "${dest_file%.py}.mpy"
+done
